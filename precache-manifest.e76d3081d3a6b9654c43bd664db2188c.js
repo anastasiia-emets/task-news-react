37759c8220c5c6abb6bcbe95db5c61b567fd2313
Anastasiia <nastia4019@gmail.com>
2020-03-09 23:26:22 +0200

    Updates

---
+++ precache-manifest.e76d3081d3a6b9654c43bd664db2188c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7d8ef897e77ceecd49aa7daebf80cd",
+    "url": "/task-news-react/build/index.html"
+  },
+  {
+    "revision": "56e74bfb15c2ea8d53e7",
+    "url": "/task-news-react/build/static/css/main.96a024e7.chunk.css"
+  },
+  {
+    "revision": "f8a82064f0a9b26a888c",
+    "url": "/task-news-react/build/static/js/2.d45dc057.chunk.js"
+  },
+  {
+    "revision": "56e74bfb15c2ea8d53e7",
+    "url": "/task-news-react/build/static/js/main.efcf4506.chunk.js"
+  },
+  {
+    "revision": "38d64422c39408084bac",
+    "url": "/task-news-react/build/static/js/runtime~main.a57c00bf.js"
+  },
+  {
+    "revision": "8282f24c2d45f6d3e7320fb7e78762ad",
+    "url": "/task-news-react/build/static/media/comment1.8282f24c.jpg"
+  },
+  {
+    "revision": "8090b58b49512a35a83e532b2aba6553",
+    "url": "/task-news-react/build/static/media/comment10.8090b58b.jpg"
+  },
+  {
+    "revision": "e34cbe13fa40e5f1e37f0c55002c4324",
+    "url": "/task-news-react/build/static/media/comment11.e34cbe13.png"
+  },
+  {
+    "revision": "4f109fdc84bdf91b0bd41e4228045767",
+    "url": "/task-news-react/build/static/media/comment12.4f109fdc.png"
+  },
+  {
+    "revision": "4613b5f53a1bd2361cce22a797797538",
+    "url": "/task-news-react/build/static/media/comment2.4613b5f5.jpg"
+  },
+  {
+    "revision": "aa8f38fde03af31aee9a704aa722bb00",
+    "url": "/task-news-react/build/static/media/comment3.aa8f38fd.jpg"
+  },
+  {
+    "revision": "a8344d768c0d394bdf377c6ef9120ae0",
+    "url": "/task-news-react/build/static/media/comment4.a8344d76.jpg"
+  },
+  {
+    "revision": "a32d7de41883a358876bd6ff783d2e26",
+    "url": "/task-news-react/build/static/media/comment5.a32d7de4.jpg"
+  },
+  {
+    "revision": "594dabe4fdec1c981625b5e49c4967da",
+    "url": "/task-news-react/build/static/media/comment6.594dabe4.jpg"
+  },
+  {
+    "revision": "72c339da4779a6ff32e08d7073d6eaae",
+    "url": "/task-news-react/build/static/media/comment7.72c339da.jpg"
+  },
+  {
+    "revision": "a26b1433fdff79cbb1067266461e3f76",
+    "url": "/task-news-react/build/static/media/comment8.a26b1433.jpg"
+  },
+  {
+    "revision": "3ec355e51d07cc4f1b765c9d51266f36",
+    "url": "/task-news-react/build/static/media/comment9.3ec355e5.jpg"
+  },
+  {
+    "revision": "f8c515be1bac956b935059c17fd5df46",
+    "url": "/task-news-react/build/static/media/img1.f8c515be.jpg"
+  },
+  {
+    "revision": "b28b2393bc186ceb019b919406501017",
+    "url": "/task-news-react/build/static/media/img2.b28b2393.png"
+  }
+]);
